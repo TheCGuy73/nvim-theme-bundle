0c951a211d7087ddefe25465cb0596f3929e8e35
unknown <thecguy73@gmail.com>
2025-06-24 17:05:44 +0200

    adding new themes

---
+++ colors/doublepink.lua
@@ -0,0 +1,36 @@
+-- DoublePink colorscheme for Neovim
+-- Palette: #2F2F2F, #DC7F9B, #F7A1C4, #E0B7B7, #94BFA7
+
+local colors = {
+  bg       = "#2F2F2F",
+  fg       = "#F7A1C4",
+  pink1    = "#DC7F9B",
+  pink2    = "#F7A1C4",
+  rose     = "#E0B7B7",
+  mint     = "#94BFA7",
+  comment  = "#94BFA7",
+  keyword  = "#DC7F9B",
+  string   = "#E0B7B7",
+  number   = "#F7A1C4",
+  error    = "#DC7F9B",
+}
+
+vim.cmd("highlight clear")
+vim.o.background = "dark"
+vim.g.colors_name = "doublepink"
+
+vim.api.nvim_set_hl(0, "Normal",      { fg = colors.fg, bg = colors.bg })
+vim.api.nvim_set_hl(0, "Comment",     { fg = colors.comment, italic = true })
+vim.api.nvim_set_hl(0, "Constant",    { fg = colors.number })
+vim.api.nvim_set_hl(0, "String",      { fg = colors.string })
+vim.api.nvim_set_hl(0, "Identifier",  { fg = colors.pink1 })
+vim.api.nvim_set_hl(0, "Function",    { fg = colors.pink2 })
+vim.api.nvim_set_hl(0, "Statement",   { fg = colors.keyword, bold = true })
+vim.api.nvim_set_hl(0, "Keyword",     { fg = colors.keyword, bold = true })
+vim.api.nvim_set_hl(0, "Type",        { fg = colors.mint })
+vim.api.nvim_set_hl(0, "Special",     { fg = colors.rose })
+vim.api.nvim_set_hl(0, "Error",       { fg = colors.error, bg = colors.bg, bold = true })
+vim.api.nvim_set_hl(0, "Todo",        { fg = colors.rose, bold = true, italic = true })
+vim.api.nvim_set_hl(0, "LineNr",      { fg = colors.mint, bg = colors.bg })
+vim.api.nvim_set_hl(0, "CursorLine",  { bg = colors.pink1 })
+vim.api.nvim_set_hl(0, "Visual",      { bg = colors.rose }) 
---
+++ colors/fire.lua
@@ -0,0 +1,36 @@
+-- Fire colorscheme for Neovim
+-- Palette: #EF7674, #EC5766, #DA344D, #D91E36, #C42348
+
+local colors = {
+  bg       = "#EF7674",
+  fg       = "#222222",
+  red1     = "#EC5766",
+  red2     = "#DA344D",
+  red3     = "#D91E36",
+  accent   = "#C42348",
+  comment  = "#EC5766",
+  keyword  = "#DA344D",
+  string   = "#C42348",
+  number   = "#D91E36",
+  error    = "#C42348",
+}
+
+vim.cmd("highlight clear")
+vim.o.background = "light"
+vim.g.colors_name = "fire"
+
+vim.api.nvim_set_hl(0, "Normal",      { fg = colors.fg, bg = colors.bg })
+vim.api.nvim_set_hl(0, "Comment",     { fg = colors.comment, italic = true })
+vim.api.nvim_set_hl(0, "Constant",    { fg = colors.number })
+vim.api.nvim_set_hl(0, "String",      { fg = colors.string })
+vim.api.nvim_set_hl(0, "Identifier",  { fg = colors.red1 })
+vim.api.nvim_set_hl(0, "Function",    { fg = colors.red2 })
+vim.api.nvim_set_hl(0, "Statement",   { fg = colors.keyword, bold = true })
+vim.api.nvim_set_hl(0, "Keyword",     { fg = colors.keyword, bold = true })
+vim.api.nvim_set_hl(0, "Type",        { fg = colors.red3 })
+vim.api.nvim_set_hl(0, "Special",     { fg = colors.accent })
+vim.api.nvim_set_hl(0, "Error",       { fg = colors.error, bg = colors.bg, bold = true })
+vim.api.nvim_set_hl(0, "Todo",        { fg = colors.accent, bold = true, italic = true })
+vim.api.nvim_set_hl(0, "LineNr",      { fg = colors.red1, bg = colors.bg })
+vim.api.nvim_set_hl(0, "CursorLine",  { bg = colors.red2 })
+vim.api.nvim_set_hl(0, "Visual",      { bg = colors.red3 }) 
